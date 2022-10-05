e84f96c71d7c7253d9682b9e205f9447c03120b9
Dubernard <lucas@derniercri.io>
2022-10-05 19:59:49 +0200

    :art: Env var

--- src/api/requests.js
+++ src/api/requests.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const getGuestsList = async () => {
   return await axios
-    .get("http://localhost:3000/api/v1/guests")
+    .get(`${process.env.REACT_APP_BASE_URL}api/v1/guests`)
     .then((res) => {
       return res.data;
     })
@@ -11,7 +11,7 @@ export const getGuestsList = async () => {
 
 export const addGuest = async (payload) => {
   return await axios
-    .post("http://localhost:3000/api/v1/guests", payload)
+    .post(`${process.env.REACT_APP_BASE_URL}api/v1/guests`, payload)
     .then((res) => {
       return res.data;
     })
@@ -20,7 +20,10 @@ export const addGuest = async (payload) => {
 
 export const updateGuest = async (payload) => {
   return await axios
-    .put(`http://localhost:3000/api/v1/guests/${payload.id}`, payload)
+    .put(
+      `${process.env.REACT_APP_BASE_URL}api/v1/guests/${payload.id}`,
+      payload
+    )
     .then((res) => {
       return res.data;
     })
@@ -29,7 +32,7 @@ export const updateGuest = async (payload) => {
 
 export const deleteGuest = async (id) => {
   return await axios
-    .delete(`http://localhost:3000/api/v1/guests/${id}`)
+    .delete(`${process.env.REACT_APP_BASE_URL}api/v1/guests/${id}`)
     .then((res) => {
       return res.data;
     })
@@ -38,7 +41,7 @@ export const deleteGuest = async (id) => {
 
 export const checkEmail = async (email) => {
   return await axios
-    .get(`http://localhost:3000/api/v1/check_email?email=${email}`)
+    .get(`${process.env.REACT_APP_BASE_URL}api/v1/check_email?email=${email}`)
     .then((res) => {
       return res.data;
     })
